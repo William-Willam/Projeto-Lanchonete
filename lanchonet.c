303c35b24344093a3d680a6a102be1c3462e80b5
joao2740 <149392154+joao2740@users.noreply.github.com>
2025-06-17 15:53:00 -0300

    menu tela

---
+++ lanchonet.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+//menu principal
+int main (){
+    printf("---------------------------------------------------------\n");
+    printf("|          Bem-vindo ao Cantinho do Donald              |\n");
+    printf("|          A seguir informe o seu Pedido                |\n");
+    printf("---------------------------------------------------------\n");
+    
+
+}
